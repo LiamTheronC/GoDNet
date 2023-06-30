7722c0d22d4851445b3e72c740454e5c36d26cbb
LiamTheronC <caij0018@e.ntu.edu.sg>
2023-06-30 09:44:16 +0800

    update training

--- working/plot.py
+++ working/plot.py
@@ -68,7 +68,7 @@ def main():
     
     config["dim_feats"] = {'xyvp':[6,2], 'xyz':[4,3], 'xy':[3,2], 'xyp':[4,2], 'vp':[4,2]}
     config['type_feats'] = 'xy'
-    config['f'] = '1f'
+    config['f'] = '10f'
     config['name'] = 'laneGCN'
     config['train_split'] = '/home/avt/prediction/Waymo/data_processed/' + config['type_feats'] + '/train_' + config['f'] 
     config['val_split'] = '/home/avt/prediction/Waymo/data_processed/' + config['type_feats'] + '/val_' + config['f']
--- working/result/print_result.py
+++ working/result/print_result.py
@@ -11,6 +11,7 @@ result['vp_1f'] = ['loss:1.54 --- fde:2.58 --- ade:1.06',
 result['xy_1f'] = 'loss:1.80 -- fde:3.07 -- ade:1.29 -- Tfde:10.74 -- Tade:4.28' #30
 result['xyvp_5f'] = 'loss:1.17 -- fde:1.91 -- ade:0.79 -- Tfde:7.23 -- Tade:2.89'
 result['vp_5f'] = 'loss:1.10 -- fde:1.72 -- ade:0.73 -- Tfde:6.56 -- Tade:2.69' # sparse spatial attention
+result['xy_10f_lanGCN'] = 'loss:0.97 -- fde:1.51 -- ade:0.65 -- Tfde:5.71 -- Tade:2.36'
 
 
 def main():
