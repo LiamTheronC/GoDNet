463d2ccae0e7fd4c56665635d65885cdbd01ecd1
LiamTheronC <caij0018@e.ntu.edu.sg>
2023-07-03 10:31:03 +0800

    update training

--- working/result/print_result.py
+++ working/result/print_result.py
@@ -12,6 +12,7 @@ result['xy_1f'] = 'loss:1.80 -- fde:3.07 -- ade:1.29 -- Tfde:10.74 -- Tade:4.28'
 result['xyvp_5f'] = 'loss:1.17 -- fde:1.91 -- ade:0.79 -- Tfde:7.23 -- Tade:2.89'
 result['vp_5f'] = 'loss:1.10 -- fde:1.72 -- ade:0.73 -- Tfde:6.56 -- Tade:2.69' # sparse spatial attention
 result['xy_10f_lanGCN'] = 'loss:0.97 -- fde:1.51 -- ade:0.65 -- Tfde:5.71 -- Tade:2.36'
+result['xy_10f_GANet'] = 'loss:2.43 -- fde:1.53 -- ade:0.68 -- Tfde:5.73 -- Tade:2.44'
 
 
 def main():
