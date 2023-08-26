76a2c69d9b731165d78c64a3d3aa7f8d205df78b
CAI JIACHENG <52328101+LiamTheronC@users.noreply.github.com>
2023-08-26 21:26:48 +0800

    Update train.py

--- working/train.py
+++ working/train.py
@@ -19,12 +19,9 @@ from losses.godnet import Loss
 
 
 def parse_args():
-  parser = argparse.ArgumentParser(description='waymo motion dataset preprocess')
-  parser.add_argument('path', choices=['train', 'val', 'test'], help='file path for the preprocess')
-  parser.add_argument('--num-scale', default=6, help='scale of predecessor and successor node pairs')
-  parser.add_argument('--cross-dist', default=6, help='cross distance for node left paris and right pairs')
-  parser.add_argument('--downsample-factor', default=10, help='downsample factor for graph')
+  parser = argparse.ArgumentParser(description='GoDNet training')
   parser.add_argument('--type-feats', choices=['vp', 'xyvp', 'xyz','xyp'], default='vp', help='types of feature')
+  parser.add_argument('--resume-from', help='the checkpoint file to resume from')
   args = parser.parse_args()
 
   return args
@@ -86,7 +83,7 @@ def val1(net, val_loader, loss_f, epoch, num_epochs, post, Tfde_a, config):
 
     if Tfde < Tfde_a:
         print('update weights')
-        torch.save(net.state_dict(), 'weights/'+ config['name'] +'_'+ config['type_feats'] + '_' + config['f'] + config['dd'] +'.pth')
+        torch.save(net.state_dict(), 'weights/'+ config['name'] +'_'+ config['type_feats'] + '_' + config['dd'] +'.pth')
         Tfde_a = Tfde
 
     return Tfde_a
@@ -94,97 +91,99 @@ def val1(net, val_loader, loss_f, epoch, num_epochs, post, Tfde_a, config):
 
 @profile
 def main():
-    args = parse_args()
+  args = parse_args()
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    working_dir = os.path.dirname(script_dir)
-    
-    seed = 33
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  working_dir = os.path.dirname(script_dir)
+  
+  seed = 33
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed(seed)
+  np.random.seed(seed)
+  random.seed(seed)
 
-    
+  
 
-    config = dict()
-    config['n_actornet'] = 128
-    config['num_epochs'] = 150
-    config['lr'] = 1e-3
-    config["num_scales"] = 6
-    config["n_map"] = 128
-    config["n_actor"] = 128
-    config['n_mark'] = 128
-    config["actor2map_dist"] = 7.0 # 7.0
-    config["map2actor_dist"] = 6.0 # 6.0
-    config["actor2actor_dist"] = 50.0
-    config["num_mods"] = 6
-    config["pred_size"] = 80
-    config["pred_step"] = 1
-    config["num_preds"] = config["pred_size"] // config["pred_step"]
-    config["cls_th"] = 2.0 #5.0, 2.0
-    config["cls_th_2"] = 0.6
-    config["cls_ignore"] = 0.2
-    config["mgn"] = 0.2
-    config["cls_coef"] = 1.0
-    config["reg_coef"] = 1.0
-    config["metrics_preds"] = 80
-    config['acrs'] = [20,40,60] # [40,80]
-    config['cut'] = range(10,50)
-    config["dim_feats"] = {'xyvp':[6,2], 'xyz':[4,3], 'xy':[3,2], 'xyp':[4,2], 'vp':[4,2], 'vpt':[5,2]}
-    config['type_feats'] = 'vp'
-    config['name'] = 'GoDNet'
-    config['train_split'] = '/home/avt/prediction/Waymo/data_processed/' + config['type_feats'] + '/train_' 
-    config['val_split'] = '/home/avt/prediction/Waymo/data_processed/' + config['type_feats'] + '/val_' 
-    config['dd'] = date.today().strftime('%m%d')
+  config = dict()
+  config['n_actornet'] = 128
+  config['num_epochs'] = 150
+  config['lr'] = 1e-3
+  config["num_scales"] = 6
+  config["n_map"] = 128
+  config["n_actor"] = 128
+  config['n_mark'] = 128
+  config["actor2map_dist"] = 7.0 # 7.0
+  config["map2actor_dist"] = 6.0 # 6.0
+  config["actor2actor_dist"] = 50.0
+  config["num_mods"] = 6
+  config["pred_size"] = 80
+  config["pred_step"] = 1
+  config["num_preds"] = config["pred_size"] // config["pred_step"]
+  config["cls_th"] = 2.0 #5.0, 2.0
+  config["cls_th_2"] = 0.6
+  config["cls_ignore"] = 0.2
+  config["mgn"] = 0.2
+  config["cls_coef"] = 1.0
+  config["reg_coef"] = 1.0
+  config["metrics_preds"] = 80
+  config['acrs'] = [20,40,60] # [40,80]
+  config['cut'] = range(10,50)
+  config["dim_feats"] = {'xyvp':[6,2], 'xyz':[4,3], 'xy':[3,2], 'xyp':[4,2], 'vp':[4,2], 'vpt':[5,2]}
+  config['type_feats'] = args.type_feats
+  config['name'] = 'GoDNet'
+  config['train_split'] = os.path.join(working_dir, 'data_processed', 'train')
+  config['val_split'] = os.path.join(working_dir, 'data_processed', 'val')
+  config['dd'] = date.today().strftime('%m%d')
 
 
-    net = GreatNet(config)
-    checkpoint = torch.load('/home/avt/prediction/Waymo/working/weights/GoDNet.pth') 
+  net = GreatNet(config)
+  if args.resume_from is not None and osp.isfile(args.resume_from):
+    resume_path = os.path.join(working_dir, 'weights', args.resume_from)
+    checkpoint = torch.load(resume_path) 
     net.load_state_dict(checkpoint)
-    net.cuda()
 
+  net.cuda()
 
-    loss_f = Loss(config)
-    loss_f.cuda()
+  loss_f = Loss(config)
+  loss_f.cuda()
 
-    post = Postprocess(config)
+  post = Postprocess(config)
 
-    optimizer = optim.Adam(net.parameters(), lr = config['lr'])
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.08)
+  optimizer = optim.Adam(net.parameters(), lr = config['lr'])
+  #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.08)
 
-    batch_size = 4
-    dataset_train = W_Dataset(config['train_split'])
-    train_loader = DataLoader(dataset_train, 
-                           batch_size = batch_size ,
-                           collate_fn = collate_fn, 
-                           shuffle = True, 
-                           drop_last=True)
-    
-    dataset_val = W_Dataset(config['val_split'])
-    val_loader = DataLoader(dataset_val, 
-                           batch_size = batch_size ,
-                           collate_fn = collate_fn, 
-                           shuffle = True, 
-                           drop_last=True)
-    
+  batch_size = 32
+  dataset_train = W_Dataset(config['train_split'])
+  train_loader = DataLoader(dataset_train, 
+                         batch_size = batch_size ,
+                         collate_fn = collate_fn, 
+                         shuffle = True, 
+                         drop_last=True)
+  
+  dataset_val = W_Dataset(config['val_split'])
+  val_loader = DataLoader(dataset_val, 
+                         batch_size = batch_size ,
+                         collate_fn = collate_fn, 
+                         shuffle = True, 
+                         drop_last=True)
+  
 
-    num_epochs = config['num_epochs']
-    
-    log_file = 'train.log'
-    log_format = '%(asctime)s - %(levelname)s - %(message)s'
-    logging.basicConfig(filename=log_file, level=logging.INFO, format=log_format)
+  num_epochs = config['num_epochs']
+  
+  log_file = 'train.log'
+  log_format = '%(asctime)s - %(levelname)s - %(message)s'
+  logging.basicConfig(filename=log_file, level=logging.INFO, format=log_format)
 
-    msg = 'Training info: ' + config['name'] + ',' + config['type_feats'] + ',' + config['f']
-    print(msg)
-    logging.info(msg)
+  msg = 'Training info: ' + config['name'] + ',' + config['type_feats']
+  print(msg)
+  logging.info(msg)
 
-    Tfde_a = 100
-    for epoch in range(num_epochs):
-        train1(net,train_loader,loss_f,optimizer,epoch,num_epochs,post)
-        #scheduler.step()
-        if (epoch + 1) % 2 == 0 or epoch == 0:
-            Tfde_a = val1(net,val_loader,loss_f,epoch,num_epochs, post, Tfde_a, config)
+  Tfde_a = 100
+  for epoch in range(num_epochs):
+      train1(net,train_loader,loss_f,optimizer,epoch,num_epochs,post)
+      #scheduler.step()
+      if (epoch + 1) % 2 == 0 or epoch == 0:
+          Tfde_a = val1(net,val_loader,loss_f,epoch,num_epochs, post, Tfde_a, config)
 
 
 if __name__ == "__main__":
