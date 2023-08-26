b1bd7a1d81ce47c806879431a0659f5dcd53fc35
CAI JIACHENG <52328101+LiamTheronC@users.noreply.github.com>
2023-08-26 22:34:00 +0800

    Update dist_train.sh

--- working/dist_train.sh
+++ working/dist_train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-CONFIG=$1
+TYPE_FEATS=$1
 GPUS=$2
-PORT=${PORT:-28509}
+PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3} --deterministic
+    $(dirname "$0")/train.py --type-feats $TYPE_FEATS --num-gpus $GPUS
